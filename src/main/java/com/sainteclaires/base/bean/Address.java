d2eb6fbf9c839c275ba323dffdaa78ab5cb2ef9f
kxmd2008 <kxmd2008@gmail.com>
2014-05-28 19:28:44 +0800

    add

---
+++ src/main/java/com/sainteclaires/base/bean/Address.java
@@ -0,0 +1,32 @@
+package com.sainteclaires.base.bean;
+
+public class Address {
+	private String loginName;
+	private String address;
+	private String postCode;// 邮编
+
+	public String getLoginName() {
+		return loginName;
+	}
+
+	public void setLoginName(String loginName) {
+		this.loginName = loginName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPostCode() {
+		return postCode;
+	}
+
+	public void setPostCode(String postCode) {
+		this.postCode = postCode;
+	}
+
+}
