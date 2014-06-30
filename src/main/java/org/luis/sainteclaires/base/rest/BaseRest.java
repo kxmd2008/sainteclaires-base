9a756edf8b81b0cbfe799e569fd1b61b5668aeaa
kxmd2008 <kxmd2008@gmail.com>
2014-06-30 20:16:06 +0800

    update

--- src/main/java/org/luis/sainteclaires/base/rest/BaseRest.java
+++ src/main/java/org/luis/sainteclaires/base/rest/BaseRest.java
@@ -9,19 +9,19 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping
 public class BaseRest {
-	@RequestMapping(value = "login", method = RequestMethod.POST)
-	@ResponseBody
-	public SimpleMessage login(String loginName, String password) {
-		SimpleMessage sm = new SimpleMessage();
-
-		return sm;
-	}
-
-	@RequestMapping(value = "logout", method = RequestMethod.POST)
-	@ResponseBody
-	public SimpleMessage logout(String loginName) {
-		SimpleMessage sm = new SimpleMessage();
-
-		return sm;
-	}
+//	@RequestMapping(value = "login", method = RequestMethod.POST)
+//	@ResponseBody
+//	public SimpleMessage login(String loginName, String password) {
+//		SimpleMessage sm = new SimpleMessage();
+//
+//		return sm;
+//	}
+//
+//	@RequestMapping(value = "logout", method = RequestMethod.POST)
+//	@ResponseBody
+//	public SimpleMessage logout(String loginName) {
+//		SimpleMessage sm = new SimpleMessage();
+//
+//		return sm;
+//	}
 }
