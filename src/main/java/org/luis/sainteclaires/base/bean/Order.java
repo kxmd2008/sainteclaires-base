725e4a0762a862714d27e0c58d481e8945f6629f
kxmd2008 <kxmd2008@gmail.com>
2014-07-21 09:49:50 +0800

    update

--- src/main/java/org/luis/sainteclaires/base/bean/Order.java
+++ src/main/java/org/luis/sainteclaires/base/bean/Order.java
@@ -17,8 +17,8 @@ public class Order extends BaseEntity {
 	 * 
 	 */
 	private static final long serialVersionUID = 6624377395219783412L;
-	@Column(length = 50)
-	private String no;// 订单号
+	@Column(length = 50, name = "order_no")
+	private String orderNo;// 订单号
 	@Column(length = 8, name = "order_date")
 	private String orderDate;// 下单日期
 	@Column(name = "order_time")
@@ -34,12 +34,12 @@ public class Order extends BaseEntity {
 	@Transient
 	private List<OrderItem> items;// 订单明细
 
-	public String getNo() {
-		return no;
+	public String getOrderNo() {
+		return orderNo;
 	}
 
-	public void setNo(String no) {
-		this.no = no;
+	public void setOrderNo(String orderNo) {
+		this.orderNo = orderNo;
 	}
 
 	public String getOrderDate() {
