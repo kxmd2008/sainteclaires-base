5fdac853894caf58161199010c3ce4263536867a
kxmd2008 <kxmd2008@gmail.com>
2014-09-12 10:20:48 +0800

    d

--- src/main/java/org/luis/sainteclaires/base/security/MySavedRequestAwareAuthenticationSuccessHandler.java
+++ src/main/java/org/luis/sainteclaires/base/security/MySavedRequestAwareAuthenticationSuccessHandler.java
@@ -5,8 +5,10 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
 import org.springframework.security.web.savedrequest.RequestCache;
@@ -18,29 +20,35 @@ public class MySavedRequestAwareAuthenticationSuccessHandler extends
 
 	private RequestCache requestCache = new HttpSessionRequestCache();
 
-	@Override
-	public void onAuthenticationSuccess(HttpServletRequest request,
-			HttpServletResponse response, Authentication authentication)
-			throws ServletException, IOException {
-		SavedRequest savedRequest = requestCache.getRequest(request, response);
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+            Authentication authentication) throws ServletException, IOException {
+        SavedRequest savedRequest = requestCache.getRequest(request, response);
+        HttpSession session = request.getSession();
+        UserDetails userDetails = (UserDetails) authentication.getPrincipal();
+        session.setAttribute("userName", userDetails.getUsername());
+        if (savedRequest == null) {
+            super.onAuthenticationSuccess(request, response, authentication);
+            return;
+        }
+        String targetUrlParameter = getTargetUrlParameter();
+        if (isAlwaysUseDefaultTargetUrl() || (targetUrlParameter != null && StringUtils.hasText(request.getParameter(targetUrlParameter)))) {
+            requestCache.removeRequest(request, response);
+            super.onAuthenticationSuccess(request, response, authentication);
 
-		if (savedRequest == null) {
-			clearAuthenticationAttributes(request);
-			return;
-		}
-		String targetUrlParam = getTargetUrlParameter();
-		if (isAlwaysUseDefaultTargetUrl()
-				|| (targetUrlParam != null && StringUtils.hasText(request
-						.getParameter(targetUrlParam)))) {
-			requestCache.removeRequest(request, response);
-			clearAuthenticationAttributes(request);
-			return;
-		}
+            return;
+        }
 
-		clearAuthenticationAttributes(request);
-	}
+        clearAuthenticationAttributes(request);
 
-	public void setRequestCache(RequestCache requestCache) {
-		this.requestCache = requestCache;
-	}
+        // Use the DefaultSavedRequest URL
+        String targetUrl = savedRequest.getRedirectUrl();
+        logger.debug("Redirecting to DefaultSavedRequest Url: " + targetUrl);
+        getRedirectStrategy().sendRedirect(request, response, targetUrl);
+    }
+
+    public void setRequestCache(RequestCache requestCache) {
+        this.requestCache = requestCache;
+    }
+	
 }
