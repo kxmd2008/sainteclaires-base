ee4d34e2b0f250a414e52c3f0ac8b14e9822f129
kxmd2008 <kxmd2008@gmail.com>
2014-09-24 16:46:44 +0800

    d

--- src/main/java/org/luis/sainteclaires/base/bean/service/OrderService.java
+++ src/main/java/org/luis/sainteclaires/base/bean/service/OrderService.java
@@ -2,16 +2,14 @@ package org.luis.sainteclaires.base.bean.service;
 
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.luis.basic.util.IbatisBuilder;
-import org.luis.sainteclaires.base.INameSpace;
 import org.luis.sainteclaires.base.bean.Order;
 import org.luis.sainteclaires.base.bean.OrderItem;
 import org.luis.sainteclaires.base.bean.ProductShot;
 import org.luis.sainteclaires.base.bean.ShoppingBag;
-import org.luis.sainteclaires.base.util.BaseUtil;
-import org.luis.sainteclaires.base.util.SequenceProvider;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,14 +17,14 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional(rollbackFor = { RuntimeException.class, Exception.class })
 public class OrderService {
 
-	public Order createOrder(ShoppingBag bag, String userName){
-		Order order = new Order();
-		order.setAccount(userName);
-		order.setAmount(bag.getTotalAmount());
-		order.setCustNo(userName);
-		order.setOrderDate(BaseUtil.getCurrDate());
-		order.setOrderTime(System.currentTimeMillis());
-		order.setOrderNo(SequenceProvider.getInstance().genericSequence(INameSpace.KEY_ORDER_NO));
+	public Order createOrder(ShoppingBag bag, String userName) {
+		//查询客户未付款订单
+		Order order = findUnpayOrder(userName);;
+		if(order == null){
+			order = new Order();
+		} else {
+			order.setAmount(order.getAmount().add(bag.getTotalAmount()));
+		}
 		boolean b = ServiceFactory.getOrderService().save(order);
 		if (!b) {
 			throw new RuntimeException("save Order error");
@@ -38,7 +36,8 @@ public class OrderService {
 			item.setProductId(shot.getProductId());
 			item.setOrderId(order.getId());
 			item.setSize(shot.getSize());
-			item.setSum(item.getPrice().multiply(BigDecimal.valueOf(item.getNum())));
+			item.setSum(item.getPrice().multiply(
+					BigDecimal.valueOf(item.getNum())));
 			order.getItems().add(item);
 			ServiceFactory.getOrderDetailService().save(item);
 		}
@@ -47,13 +46,37 @@ public class OrderService {
 		}
 		return order;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public List<Order> findUnpayOrders(String userName){
+	public Order findUnpayOrder(String userName) {
 		List<Order> list = null;
 		try {
 			list = (List<Order>) IbatisBuilder.queryForList(
 					"order.findUnpayOrders", userName);
+			for (Order order : list) {
+				for (OrderItem item : order.getItems()) {
+					String[] pics = item.getPic().split(",");
+					item.setPic(pics[0]);
+				}
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		Order order = null;
+		if(list == null || list.isEmpty()){
+			return null;
+		} else {
+			order = list.get(0);
+		}
+		return order;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Order> findOrders(String userName) {
+		List<Order> list = null;
+		try {
+			list = (List<Order>) IbatisBuilder.queryForList(
+					"order.findOrders", userName);
 			for (Order order : list) {
 				for (OrderItem item : order.getItems()) {
 					String[] pics = item.getPic().split(",");
