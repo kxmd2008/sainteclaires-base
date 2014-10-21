15d1946e52ab4c65ab19b93bc7e4c01c8f498048
kxmd2008 <kxmd2008@gmail.com>
2014-10-21 16:38:20 +0800

    d

---
+++ src/main/java/org/luis/sainteclaires/base/BaseInit.java
@@ -0,0 +1,21 @@
+package org.luis.sainteclaires.base;
+
+import javax.annotation.PostConstruct;
+
+import org.luis.basic.domain.FilterAttributes;
+import org.luis.sainteclaires.base.bean.Config;
+import org.luis.sainteclaires.base.bean.service.ServiceFactory;
+import org.luis.sainteclaires.base.util.BaseUtil;
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseInit {
+
+	@PostConstruct
+	public void init() {
+		FilterAttributes fa = FilterAttributes.blank().add("key", "quarter")
+				.add("quarter", "QUARTER");
+		Config config = ServiceFactory.getConfigService().findOneByFilter(fa);
+		BaseUtil.setCurrQuarter(config);
+	}
+}
